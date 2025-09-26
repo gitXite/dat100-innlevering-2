e961506b33802948aacc43056e06fa6ebb6b68b4
Daniel <Daniel.halaas@gmail.com>
2025-09-26 20:51:03 +0200

    Implementere skrivUt(), tilStreng(), og skaler() metoder

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,25 +4,43 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		System.out.println("----------");
+		for (int[] tabell : matrise) {
+			for (int tall : tabell) {
+				System.out.print(tall + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("----------");
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
+		StringBuilder sb = new StringBuilder();
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+				if (j < matrise[i].length - 1) {
+					sb.append(matrise[i][j] + " ");
+				} else {
+					sb.append(matrise[i][j] + " \n");
+				}
+			}
+		}
+		return sb.toString();
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		if (matrise == null || matrise.length == 0 || matrise[0].length == 0) return new int[0][0];
+		int[][] resMatrise = new int[matrise.length][matrise[0].length];
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+				resMatrise[i][j] = matrise[i][j] * tall;
+			}
+		}
+		return resMatrise;
 	}
 
 	// d)
